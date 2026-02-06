eb5a97d69c294691a6298aefc5e7d3783c95dad1
Quick CRM Developer <dev@quickcrm.local>
2026-02-06 19:19:36 +0530

    fix: prevent page-level horizontal scroll on mobile by adding min-w-0 to main layout

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -88,7 +88,7 @@ const Layout = () => {
             </aside>
 
             {/* Main Content */}
-            <main className="flex-1 lg:ml-64 p-4 sm:p-6 lg:p-8 mt-16 lg:mt-0">
+            <main className="flex-1 lg:ml-64 p-4 sm:p-6 lg:p-8 mt-16 lg:mt-0 min-w-0">
                 <div className="max-w-6xl mx-auto">
                     <Outlet />
                 </div>
